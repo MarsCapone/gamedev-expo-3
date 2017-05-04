a06a3debd990bb18840a635814432e9b24c69eb4
Samson Danziger <samson.danziger@gmail.com>
2017-05-04 11:35:46 +0100

    add rate update to ping

--- Arduino/HRSensor.ino
+++ Arduino/HRSensor.ino
@@ -3,6 +3,13 @@
 SerialCommand sCmd;
 int DELAY_TIME = 50;
 
+int PURPLE_PIN = 0;
+int THRESHHOLD = 550;
+int hrSignal; // value: 0 - 1024
+
+int lastTime;
+float rate;
+
 void setup() {
     Serial.begin(9600);
     while (!Serial);
@@ -10,19 +17,30 @@ void setup() {
     sCmd.addCommand("PING", pingHandler);
     sCmd.addCommand("ECHO", echoHandler);
     sCmd.setDefaultHandler(errorHandler);
+
+    lastTime = 0;
 }
 
 void loop() {
     if (Serial.available > 0) {
         sCmd.readSerial();
     }
+    
+    hrSignal = analogRead(PURPLE_PIN);
+    if (hrSignal > THRESHHOLD) {
+        if (lastTime > 0) {
+            rate = getRate(lastTime);
+        }
+        lastTime = 0;
+    }
 
+    lastTime += DELAY_TIME;
     delay(DELAY_TIME);
 }
 
 void pingHandler() {
-    Serial.println("PONG");
-    // should send HR info here
+    //Serial.println("PONG");
+    Serial.println(rate);
 }
 
 void echoHandler() {
@@ -39,4 +57,7 @@ void errorHandler(const char *command) {
     // error handling
 }
 
+float getRate(int msBetweenRR) {
+    rate = 60 / (msBetweenRR / 1000);
+}
 
