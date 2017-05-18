58d1a2d49b2eb361538e5c66ce927c9fabfd2dfb
Samson Danziger <samson.danziger@gmail.com>
2017-05-18 11:52:46 +0100

    add tutorial level layout

---
+++ Assets/_Scripts/RemoveLighting.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RemoveLighting : MonoBehaviour {
+
+    public float lightLevel = 0.01f;
+
+	// Use this for initialization
+	void Start () {
+        Light[] allLights = gameObject.GetComponentsInChildren<Light>();
+        for (int i = 0; i < allLights.Length; i++)
+        {
+            Light light = allLights[i];
+            light.intensity = lightLevel;
+        }
+	}
+}
