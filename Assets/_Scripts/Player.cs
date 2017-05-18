3ae7e2d19c0622a29fd335245708065911e39a04
Samson Danziger <samson.danziger@gmail.com>
2017-05-18 18:55:20 +0100

    add basic player health stuff

--- Assets/_Scripts/Player.cs
+++ Assets/_Scripts/Player.cs
@@ -3,6 +3,11 @@ using System.Collections;
 
 public class Player : MonoBehaviour
 {
+    public int Health = 10;
+
+    public delegate void HealthUpdate(int health);
+    public static event HealthUpdate OnHealthUpdate;
+
     public Vector3 GetPosition()
     {
         return gameObject.transform.position;
@@ -11,5 +16,31 @@ public class Player : MonoBehaviour
     public void SetPosition(Vector3 position)
     {
         gameObject.transform.position = position;
+    }
+
+    void Update()
+    {
+        if (OnHealthUpdate != null) OnHealthUpdate(Health);
+        if (Health < 0)
+        {
+            // ur dead
+            Die();
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Zombie")
+        {
+            // being attacked
+            Health--;
+
+        }
+    }
+
+    private void Die()
+    {
+        print("Ur dead now!!!");
+        // some other stuff
     }
 }
