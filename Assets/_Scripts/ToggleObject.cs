c3f8894d738ae7643e720799b6a7f479d64d776a
Samson Danziger <samson.danziger@gmail.com>
2017-05-18 18:35:01 +0100

    add latest changes

---
+++ Assets/_Scripts/ToggleObject.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleObject : MonoBehaviour {
+
+    public GameObject toggleableObject;
+    public KeyCode toggleButton = KeyCode.Escape;
+    public bool showAfterCalibration = true;
+
+    private bool showing = false;
+
+    void Awake()
+    {
+        Countdown.OnCalibrationComplete += Countdown_OnCalibrationComplete;
+        MonitorDisplay.OnAbsoluteMax += MonitorDisplay_OnAbsoluteMax;
+    }
+
+    private void MonitorDisplay_OnAbsoluteMax(bool b)
+    {
+        if (b) Show();
+    }
+
+    private void Countdown_OnCalibrationComplete()
+    {
+        Show();
+    }
+
+    // Update is called once per frame
+    void Update () {
+		if (Input.GetKeyDown(toggleButton))
+        {
+            showing = !showing;
+        }
+
+        if (showing)
+        {
+            Time.timeScale = 0; // pause 
+        } else
+        {
+            Time.timeScale = 1;
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+
+        toggleableObject.SetActive(showing);
+	}
+
+    public void SetShowing(bool b)
+    {
+        showing = b;
+    }
+
+    public void Show()
+    {
+        showing = true;
+    }
+
+    public void Hide()
+    {
+        showing = false;
+    }
+}
---
+++ Assets/_Scripts/Zombie.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zombie : MonoBehaviour {
+
+    public int MinHitsToDeath = 1;
+    public int MaxHitsToDeath = 3;
+
+    public AudioClip AttackSound;
+    public AudioClip DeadSound;
+
+    private int hitsToDeath;
+    private int currentHits = 0;
+    private bool isInstaDeath = false;
+    private AudioSource audioSource;
+
+    void Awake()
+    {
+        MonitorDisplay.OnInstaDeath += HandleOnInstaDeath;
+    }
+
+    // Use this for initialization
+    void Start () {
+        hitsToDeath = Random.Range(MinHitsToDeath, MaxHitsToDeath);
+        audioSource = GetComponent<AudioSource>();
+	}
+	
+	
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Weapon")
+        {
+            // being hit 
+            if (!isInstaDeath)
+            {
+                // attacks work
+                audioSource.clip = AttackSound;
+                audioSource.Play();
+                currentHits++;
+
+                if (currentHits == hitsToDeath)
+                {
+                    // die
+                    gameObject.SetActive(false);
+                }
+            }
+        }
+    }
+
+    private void HandleOnInstaDeath(bool b)
+    {
+        isInstaDeath = b;
+    }
+}
